5a36cdc15b431e5551298561d393644dada59780
Hiroki UCHIDA <nikuyoshi@gmail.com>
2015-08-18 23:51:38 +0900

    Fix HttpClient

--- dmm4j-core/src/main/java/dmm4j/http/HttpClient.java
+++ dmm4j-core/src/main/java/dmm4j/http/HttpClient.java
@@ -1,39 +1,43 @@
 /*
  * Copyright 2015 Hiroki Uchida
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package dmm4j.http;
 
+
 import java.util.Map;
 
+import dmm4j.DmmException;
+
 /**
- * @author nikuyoshi
+ * A utility class to handle HTTP request/response.
+ *
  */
 public interface HttpClient {
-    Map<String, String> getRequestHeaders();
 
-    HttpResponse request(HttpRequest req) ;
+  void addDefaultRequestHeader(String name, String value);
 
-    HttpResponse get(String url) ;
+  Map<String, String> getRequestHeaders();
 
-    HttpResponse post(String url) ;
+  HttpResponse request(HttpRequest req) throws DmmException;
 
-    HttpResponse delete(String url) ;
+  HttpResponse get(String url) throws DmmException;
 
-    HttpResponse head(String url) ;
+  HttpResponse post(String url) throws DmmException;
 
-    HttpResponse put(String url) ;
+  HttpResponse delete(String url) throws DmmException;
 
+  HttpResponse head(String url) throws DmmException;
+
+  HttpResponse put(String url) throws DmmException;
 }
