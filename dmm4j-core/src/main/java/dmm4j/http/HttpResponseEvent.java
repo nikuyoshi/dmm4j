8a7db54bd6d833442ffdee14ff5852a0f4dd6573
Hiroki UCHIDA <nikuyoshi@gmail.com>
2015-09-13 20:12:04 +0900

    Add HttpResponseEvent

---
+++ dmm4j-core/src/main/java/dmm4j/http/HttpResponseEvent.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2015 Hiroki Uchida
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package dmm4j.http;
+
+import dmm4j.DmmException;
+
+
+public final class HttpResponseEvent {
+
+  private final HttpRequest request;
+
+  private final HttpResponse response;
+
+  private final DmmException dmmException;
+
+  HttpResponseEvent(HttpRequest request, HttpResponse response, DmmException de) {
+    this.request = request;
+    this.response = response;
+    this.dmmException = de;
+  }
+
+  /**
+   * returns the request associated with the event
+   *
+   * @return the request associated with the event
+   */
+  public HttpRequest getRequest() {
+    return request;
+  }
+
+  /**
+   * returns the response associated with the event
+   *
+   * @return the response associated with the event
+   */
+  public HttpResponse getResponse() {
+    return response;
+  }
+
+  /**
+   * returns the DmmException associated with the event
+   *
+   * @return the DmmException associated with the event
+   */
+  public DmmException getDmmException() {
+    return dmmException;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+
+    HttpResponseEvent that = (HttpResponseEvent) o;
+
+    if (request != null ? !request.equals(that.request) : that.request != null)
+      return false;
+    if (response != null ? !response.equals(that.response) : that.response != null)
+      return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = request != null ? request.hashCode() : 0;
+    result = 31 * result + (response != null ? response.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "HttpResponseEvent{" + "request=" + request + ", response=" + response + '}';
+  }
+}
